9128b2ffa24cb2e125d2bf47d76c3f650136a12f
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-06-14 10:51:35 +0100

    Making the Gallery Responsive

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GallaryRow, GalleryImage, GalleryWrapper } from "./Gallery.styles";
+
 import mobileImage1 from "../../images/mobile/image-gallery-milkbottles.jpg";
 import mobileImage2 from "../../images/mobile/image-gallery-orange.jpg";
 import mobileImage3 from "../../images/mobile/image-gallery-cone.jpg";
 import mobileImage4 from "../../images/mobile/image-gallery-sugar-cubes.jpg";
 
+import desktopImage1 from "../../images/desktop/image-gallery-milkbottles.jpg";
+import desktopImage2 from "../../images/desktop/image-gallery-orange.jpg";
+import desktopImage3 from "../../images/desktop/image-gallery-cone.jpg";
+import desktopImage4 from "../../images/desktop/image-gallery-sugarcubes.jpg";
+
 const Gallery = () => {
+  const [desktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 950) {
+      setDesktop(true);
+    }
+  }, []);
+
   return (
     <GalleryWrapper>
       <GallaryRow>
-        <GalleryImage src={mobileImage1} />
-        <GalleryImage src={mobileImage2} />
+        {desktop ? (
+          <>
+            <GalleryImage src={desktopImage1} />
+            <GalleryImage src={desktopImage2} />
+          </>
+        ) : (
+          <>
+            <GalleryImage src={mobileImage1} />
+            <GalleryImage src={mobileImage2} />
+          </>
+        )}
       </GallaryRow>
       <GallaryRow>
-        <GalleryImage src={mobileImage3} />
-        <GalleryImage src={mobileImage4} />
+        {desktop ? (
+          <>
+            <GalleryImage src={desktopImage3} />
+            <GalleryImage src={desktopImage4} />
+          </>
+        ) : (
+          <>
+            <GalleryImage src={mobileImage3} />
+            <GalleryImage src={mobileImage4} />
+          </>
+        )}
       </GallaryRow>
     </GalleryWrapper>
   );
--- src/components/Gallery/Gallery.styles.js
+++ src/components/Gallery/Gallery.styles.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const GalleryWrapper = styled.section`
   display: flex;
   flex-direction: column;
+
+  @media screen and (min-width: 950px) {
+    flex-direction: row;
+  }
 `;
 
 export const GalleryImage = styled.img`
